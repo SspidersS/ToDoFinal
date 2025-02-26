3cdebd6387cebcf16466511f4985f376e9ca39cf
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-26 16:05:16 +0100

    d

--- main/java/org/example/todolist/repositories/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.todolist.repositories;
-
-import org.example.todolist.enteties.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-
-}
