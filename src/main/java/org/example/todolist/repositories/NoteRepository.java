4255a86ce985000553c7190f832357147ba7765e
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-26 18:39:20 +0100

    d

--- src/main/java/org/example/todolist/repositories/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.todolist.repositories;
-
-import org.example.todolist.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-
-}
--- src/main/resources/db/migration/V2__add_default_user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username, password, enabled) VALUES ('user', 'jdbcDefault', true);
-
-INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
